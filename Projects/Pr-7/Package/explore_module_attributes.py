385b3318b7404c6354cf8e323b3e5bb3f3d3fb2f
Pranjali-Pandya <pranjalipandya11@gmail.com>
2026-06-30 00:44:58 +0530

    explore_module_attributes.py

---
+++ Projects/Pr-7/Package/explore_module_attributes.py
@@ -0,0 +1,11 @@
+import math
+def explore_module_attributes():
+    print("\n**********************Explore Module Attributes*******************************") 
+    print(dir(math))
+    print("\n*******************Thankyou for using this module**********************")
+    
+if __name__ == "__main__":
+    print("Inside package running")
+    explore_module_attributes()
+
+    
