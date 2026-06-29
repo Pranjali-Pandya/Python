e44b3eb0bd58750259cb9e8d8608b4a72325ca5d
Pranjali-Pandya <pranjalipandya11@gmail.com>
2026-06-30 00:48:22 +0530

    uuid_module.py

---
+++ Projects/Pr-7/Package/uuid_module.py
@@ -0,0 +1,9 @@
+import uuid
+def uuid_function():
+    print("Generated UUID: ",uuid.uuid4())
+    print("\n*********************Thankyou for using UUID Module***********************")
+    
+if __name__ == "__main__":
+    print("Inside package running")
+    uuid_function()
+    
